6d26816166fbcbe1c06bae4450c06bbc858c76c2
Some1onearth <chris_lee@outlook.com.au>
2025-01-23 20:07:12 +1100

    Quickly added quit buttons

--- Levels/stage_1.gd
+++ Levels/stage_1.gd
@@ -13,6 +13,10 @@ func distance_to_earth():
 func _process(delta: float) -> void:
 	%DistanceLabel.text = "Distance to Earth: %02d AU" %distance_to_earth()
 	%ScoreLabel.text = str(Global.score)
+	
+	if Input.is_action_just_pressed("Pause"):
+		%GameOver.visible = true
+		get_tree().paused = true
 
 
 func _on_countdown_timeout() -> void:
@@ -22,9 +26,20 @@ func _on_countdown_timeout() -> void:
 
 func _on_player_health_depleted() -> void:
 	%GameOver.visible = true
+	$GameOver/ColorRect/Retry.visible = true
+	$GameOver/ColorRect/Resume.visible = false
 	get_tree().paused = true
 
 
 func _on_retry_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_resume_pressed() -> void:
+	%GameOver.visible = false
+	get_tree().paused = false
