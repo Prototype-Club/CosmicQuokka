b0e3824f1b9f01a4f8831e16107f1d8f859a553c
Some1onearth <chris_lee@outlook.com.au>
2025-01-24 15:25:00 +1100

    Animation fixes

---
+++ Levels/outtro.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	$"Main Menu".visible = true
+	$Quit.visible = true
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/main_menu.tscn")
