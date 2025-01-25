fcf47f3d109e3f8f583d3fe435e92f726b6fe584
Some1onearth <chris_lee@outlook.com.au>
2025-01-26 01:35:29 +1100

    added credits

--- Levels/main_menu.gd
+++ Levels/main_menu.gd
@@ -7,3 +7,11 @@ func _on_start_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	$CreditsWindow.visible = true
+
+
+func _on_back_pressed() -> void:
+	$CreditsWindow.visible = false
